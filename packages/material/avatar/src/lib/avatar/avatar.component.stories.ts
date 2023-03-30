1c0a035bed3284682f0ab25a66ca644b5eaa5427
Andrew Alderson <andrew.alderson@outlook.com>
2023-03-29 19:14:15 -0600

    add initial avatar component story

---
+++ packages/material/avatar/src/lib/avatar/avatar.component.stories.ts
@@ -0,0 +1,15 @@
+import { Meta } from '@storybook/angular';
+import { AvatarComponent } from './avatar.component';
+
+export default {
+  title: 'AvatarComponent',
+  component: AvatarComponent
+} as Meta<AvatarComponent>;
+
+export const Primary = {
+  render: (args: AvatarComponent) => ({
+    props: args,
+  }),
+  args: {
+  },
+};
