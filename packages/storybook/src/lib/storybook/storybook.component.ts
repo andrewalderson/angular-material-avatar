2de21cb7c1e14c266cc918c2b81e0a15588bcaeb
Andrew Alderson <andrew.alderson@outlook.com>
2023-03-29 19:02:52 -0600

    add global storybook library

---
+++ packages/storybook/src/lib/storybook/storybook.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-storybook',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>storybook works!</p> `,
+  styles: [],
+})
+export class StorybookComponent {}
