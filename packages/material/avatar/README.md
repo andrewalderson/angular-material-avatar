25bf9393ecfc30a642039e6ec097cfad650b5e6e
Andrew Alderson <andrew.alderson@outlook.com>
2023-03-29 18:51:24 -0600

    add material avatar library with initial component

---
+++ packages/material/avatar/README.md
@@ -0,0 +1,7 @@
+# material-avatar
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test material-avatar` to execute the unit tests.
---
+++ packages/material/avatar/src/lib/avatar/avatar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'matx-avatar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AvatarComponent {}
