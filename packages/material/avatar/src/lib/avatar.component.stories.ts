b82141d94a311c739065b317a49a84a11f67a81b
Andrew Alderson <andrew.alderson@outlook.com>
2023-09-25 01:41:41 +0000

    style(avatar): add border styles

--- packages/material/avatar/src/lib/avatar.component.stories.ts
+++ packages/material/avatar/src/lib/avatar.component.stories.ts
@@ -1,5 +1,9 @@
-import { Meta } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular';
 import { MatxAvatarComponent } from './avatar.component';
+
+type StoryArgTypes = MatxAvatarComponent & {
+  borderWidth: number;
+};
 
 export default {
   title: 'Components/Avatar',
@@ -7,11 +11,21 @@ export default {
   parameters: {
     layout: 'centered',
   },
-} as Meta<MatxAvatarComponent>;
+} as Meta<StoryArgTypes>;
 
-export const WithDefaultFallback = {
-  render: (args: MatxAvatarComponent) => ({
-    props: args,
-  }),
-  args: {},
+const calculateStyles = (args: StoryArgTypes) => {
+  let style = '';
+  if (args.borderWidth) {
+    style += `--matx-avatar-border-width: ${args.borderWidth}px`;
+  }
+  return style;
+};
+
+const Template: Story<StoryArgTypes> = (args: StoryArgTypes) => ({
+  template: `<matx-avatar style="${calculateStyles(args)}" />`,
+});
+
+export const WithDefaultFallback = Template.bind(this);
+WithDefaultFallback.args = {
+  borderWidth: 0,
 };
